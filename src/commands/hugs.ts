07ba30a7b26653599de1e60fb41fa90dc3ca8e98
bricked <git@bricked.dev>
2023-09-14 21:41:30 +0200

    feat: add list hugs command

--- src/commands/hugs.ts
+++ src/commands/hugs.ts
@@ -1,12 +1,20 @@
 import { ApplyOptions } from "@sapphire/decorators";
 import { Subcommand } from "@sapphire/plugin-subcommands";
+import {
+  LazyPaginatedMessage,
+  PaginatedMessageMessageOptionsUnion,
+} from "@sapphire/discord.js-utilities";
 import { Hug } from "../schemata/Hugs";
-import { EmbedBuilder } from "discord.js";
+import { EmbedBuilder, UserResolvable, bold, userMention } from "discord.js";
+import { PAGE_LEN } from "../consts";
 
 @ApplyOptions<Subcommand.Options>({
   name: "hugs",
   description: "Manage hugs",
-  subcommands: [{ name: "give", chatInputRun: "chatInputGive" }],
+  subcommands: [
+    { name: "give", chatInputRun: "chatInputGive" },
+    { name: "list", chatInputRun: "chatInputList" },
+  ],
 })
 export class UserCommand extends Subcommand {
   public override registerApplicationCommands(registry: Subcommand.Registry) {
@@ -22,6 +30,9 @@ export class UserCommand extends Subcommand {
               option.setName("target").setDescription("Whom to hug").setRequired(true)
             )
         )
+        .addSubcommand((command) =>
+          command.setName("list").setDescription("List the most hugged members")
+        )
     );
   }
 
@@ -36,7 +47,47 @@ export class UserCommand extends Subcommand {
     hug.amount++;
     await hug.save();
 
-    const embed = new EmbedBuilder().setDescription(`You have given ${inter.user} a hug!`);
-    return inter.reply({ embeds: [embed] });
+    const embed = new EmbedBuilder()
+      .setDescription(`You have given ${inter.user} a hug 🫂!`)
+      .setColor(inter.client.color);
+    return inter.reply({ embeds: [embed], ephemeral: true });
   }
+
+  public async chatInputList(inter: Subcommand.ChatInputCommandInteraction) {
+    const hugsCount = await Hug.count({ guildId: inter.guildId });
+
+    const pages = Array.from({ length: Math.ceil(hugsCount / PAGE_LEN) }, (_v, i) => async () => {
+      const offset = PAGE_LEN * i;
+      const padding = formatIndex(Math.min(offset + PAGE_LEN, hugsCount)).length;
+      const hugs = await Hug.find({ guildId: inter.guildId }).limit(PAGE_LEN).skip(offset);
+
+      const page: PaginatedMessageMessageOptionsUnion = {
+        embeds: [
+          new EmbedBuilder()
+            .setDescription(
+              hugs
+                .map((hug, j) => formatEntry(offset + j, hug.userId, `${hug.amount} 🫂`, padding))
+                .join("\n")
+            )
+            .setColor(inter.client.color),
+        ],
+      };
+
+      return page;
+    });
+
+    const paginated = new LazyPaginatedMessage();
+    paginated.addPages(pages);
+    await paginated.run(inter);
+  }
+}
+
+function formatIndex(index: number, padding: number = 0) {
+  return bold(`${index + 1}.`.padStart(padding));
+}
+
+function formatEntry(index: number, user: UserResolvable, value?: string, padding?: number) {
+  const num = formatIndex(index, padding);
+  const mention = userMention(typeof user == "string" ? user : user.id);
+  return `${num} ${mention} ${value}`;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
+import { GatewayIntentBits, Partials } from "discord.js";
 import { Client } from "./client";
 import "dotenv/config";
 
 const client = new Client({
   baseUserDirectory: __dirname,
-  intents: [], // insert bot intents
-  partials: [], // insert partial events
+  intents: [GatewayIntentBits.Guilds], // insert bot intents
+  partials: [Partials.Channel], // insert partial events
 
   color: "Blurple", // insert an accent color
+  typing: true,
 });
 
 client.login(process.env.TOKEN);
